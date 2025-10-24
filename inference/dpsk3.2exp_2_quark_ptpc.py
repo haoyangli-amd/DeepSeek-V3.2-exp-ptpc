098cd61bc43a6fa79a8a9ddc93a9b46a22f0ee01
Haoyang Li <lihaoyang0109@gmail.com>
2025-10-24 03:39:58 +0000

    quantize deepseekv3.2-exp by QDQ safetensors with quark
    Signed-off-by: Haoyang Li <lihaoyang0109@gmail.com>

--- inference/dpsk3.2exp_2_quark_ptpc.py
+++ inference/dpsk3.2exp_2_quark_ptpc.py
@@ -6,15 +6,12 @@ from tqdm import tqdm
 
 import torch
 from safetensors.torch import load_file, save_file
-import sys
-sys.path.append(os.path.abspath("/mnt/raid0/haoyanli/lhy_quark/Quark/quark"))
 import quark
 from quark.torch.quantization.tensor_quantize import FakeQuantizeBase
 from quark.torch.quantization.tensor_quantize import FakeQuantizeBase
 from quark.torch.quantization import FP8E4M3PerChannelSpec
 FP8_PER_CHANNEL_SPEC = FP8E4M3PerChannelSpec(is_dynamic=False, ch_axis=0).to_quantization_spec()
 qspec=FP8_PER_CHANNEL_SPEC
-
 
 from kernel import weight_dequant
 
@@ -49,6 +46,7 @@ def main(fp8_path, bf16_path):
     loaded_files = {}
     fp8_weight_names = []
 
+
     # Helper function to get tensor from the correct file
     def get_tensor(tensor_name):
         """
@@ -78,65 +76,44 @@ def main(fp8_path, bf16_path):
         
         new_state_dict = {}
         for weight_name, weight in current_state_dict.items():
-            if weight_name.endswith("_scale_inv"): # we don't save weight_scale_inv, just save weight.scale in following code
+            # we don't save weight_scale_inv here, just save weight.scale in following code
+            if weight_name.endswith("_scale_inv"): 
                 continue
-            elif weight.element_size() == 1:  # FP8 weight
+            elif weight.element_size() == 1:  # FP8 weight with weight_scale_inv
                 scale_inv_name = f"{weight_name}_scale_inv"
-                if 0:
-                    try:
-                        # Get scale_inv from the correct file
-                        scale_inv = get_tensor(scale_inv_name)
-                        fp8_weight_names.append(weight_name)
-                        new_state_dict[weight_name] = weight_dequant(weight, scale_inv)
-                    except KeyError:
-                        print(f"Warning: Missing scale_inv tensor for {weight_name}, skipping conversion")
-                        new_state_dict[weight_name] = weight
-                else:
-                    if 0: # save weight and scale, and set weight_map
-                    # if "experts." in weight_name: # save weight and scale, and set weight_map
-                        new_state_dict[weight_name] = weight
-                        # if scale in thie file, save it
-                        # if other file, save it here, and don't save scale in that file dict
-                        new_state_dict[scale_inv_name] = get_tensor(scale_inv_name)
-                        if scale_inv_name in weight_map: # clear weight_map[weight: *safetensors, scale: *safetensors]
-                            weight_map.pop(scale_inv_name)
-                            weight_map[scale_inv_name] = safetensor_file.split("/")[-1]
-                    else: # If not moe, dequant to bf16 and real quant to fp8 pt
-                        scale_name = f"{weight_name}_scale"
-                        try:
-                            # Get scale_inv from the correct file
-                            scale_inv = get_tensor(scale_inv_name)
-                            fp8_weight_names.append(weight_name)
-                            # new_state_dict[weight_name][0,0:2]/scale_inv[0,0] == weight[0,0:2]
-                            bf_weight = weight_dequant(weight, scale_inv)
-                            # QDQ
-                            _weight_quantizer = FakeQuantizeBase.get_fake_quantize(qspec, device="cuda")
-                            bf_weight = _weight_quantizer(bf_weight)
-                            assert isinstance(bf_weight, torch.Tensor)
-                            # real Q
-                            dtype = qspec.dtype.value
-                            param = bf_weight
-                            scale = _weight_quantizer.scale
-                            zero_point = _weight_quantizer.zero_point
-                            ch_axis = qspec.ch_axis
-                            group_size = qspec.group_size
-                            quant_min = -448
-                            quant_max = 448
-                            round_method = getattr(qspec.round_method, "value", None)
-                            qscheme_str_name = getattr(qspec.qscheme, "value", None)
-                            # realq
-                            w_res = quark.torch.kernel.scaled_real_quantize(dtype, param, scale, zero_point, ch_axis, group_size, quant_min, quant_max, round_method, qscheme_str_name)
-                            if scale_inv_name in weight_map: # clear weight_map[weight: *safetensors, scale: *safetensors]
-                                which_safe_file = weight_map[scale_inv_name]
-                                weight_map.pop(scale_inv_name)
-                                weight_map[scale_name] = which_safe_file
-                            new_state_dict[weight_name] = w_res
-                            new_state_dict[scale_name] = scale
-                        except KeyError:
-                            print(f"Warning: Missing scale_inv tensor for {weight_name}, skipping conversion")
-                            new_state_dict[weight_name] = weight
-
-
+                quark_scale_name = f"{weight_name}_scale"
+                try:
+                    # Get scale_inv from the correct file
+                    scale_inv = get_tensor(scale_inv_name)
+                    fp8_weight_names.append(weight_name)
+                    # DQ
+                    bf_weight = weight_dequant(weight, scale_inv)
+                    _weight_quantizer = FakeQuantizeBase.get_fake_quantize(qspec, device="cuda")
+                    bf_weight = _weight_quantizer(bf_weight)
+                    assert isinstance(bf_weight, torch.Tensor)
+                    # Q
+                    dtype = qspec.dtype.value
+                    param = bf_weight
+                    scale = _weight_quantizer.scale
+                    zero_point = _weight_quantizer.zero_point
+                    ch_axis = qspec.ch_axis
+                    group_size = qspec.group_size
+                    quant_min = -448
+                    quant_max = 448
+                    round_method = getattr(qspec.round_method, "value", None)
+                    qscheme_str_name = getattr(qspec.qscheme, "value", None)
+                    w_res = quark.torch.kernel.scaled_real_quantize(dtype, param, scale, zero_point, ch_axis, group_size, quant_min, quant_max, round_method, qscheme_str_name)
+                    if scale_inv_name in weight_map: # clear weight_map[weight: *safetensors, scale: *safetensors]
+                        # which_safe_file = weight_map[scale_inv_name]
+                        # If the scale is obtained from another safe(s2), then delete the scale from s2 (achieved by scale_inv continue during traversal of s2);
+                        # And in the weight_map, change the mapping from scale_inv and the safe file s2 to quark_scale and the current safe file.
+                        weight_map.pop(scale_inv_name) 
+                        weight_map[quark_scale_name] = file_name
+                    new_state_dict[weight_name] = w_res
+                    new_state_dict[quark_scale_name] = scale
+                except KeyError:
+                    print(f"Warning: Missing scale_inv tensor for {weight_name}, skipping conversion")
+                    new_state_dict[weight_name] = weight
             else:
                 new_state_dict[weight_name] = weight
                 
@@ -149,12 +126,15 @@ def main(fp8_path, bf16_path):
             del loaded_files[oldest_file]
             torch.cuda.empty_cache()
     
+    # After traversing all safe elements, proceed to create the weight_map.
     # Update model index
     new_model_index_file = os.path.join(bf16_path, "model.safetensors.index.json")
-    if 0:
+    # Perform a secondary check to ensure scale_inv has been deleted.
+    if 0: 
         for weight_name in fp8_weight_names:
             scale_inv_name = f"{weight_name}_scale_inv"
             if scale_inv_name in weight_map:
+                print("warinng: there is a scale_inv")
                 weight_map.pop(scale_inv_name)
     with open(new_model_index_file, "w") as f:
         json.dump({"metadata": {}, "weight_map": weight_map}, f, indent=2)
@@ -163,6 +143,6 @@ def main(fp8_path, bf16_path):
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("--input-fp8-hf-path", type=str, default="/mnt/raid0/pretrained_model/deepseek-ai/DeepSeek-V3.2-Exp")
-    parser.add_argument("--output-bf16-hf-path", type=str, default="/mnt/raid0/pretrained_model/deepseek-ai/DeepSeek-V3.2-Exp-out")
+    parser.add_argument("--output-fp8-hf-path-ptpc", type=str, default="/mnt/raid0/pretrained_model/deepseek-ai/DeepSeek-V3.2-Exp-ptpc")
     args = parser.parse_args()
-    main(args.input_fp8_hf_path, args.output_bf16_hf_path)
+    main(args.input_fp8_hf_path, args.output_fp8_hf_path_ptpc)
